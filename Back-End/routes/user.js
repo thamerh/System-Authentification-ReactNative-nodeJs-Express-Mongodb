3d63e4758661a3b66542ae68269c0f89d39ce11c
thamerh <hamdithamer99@gmail.com>
2022-11-14 22:32:41 +0100

    feat: create express router for regroup all user features

---
+++ Back-End/routes/user.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const router = express.Router();
+const {
+  createUser,
+  userSignIn,
+  uploadProfile,
+  signOut,
+} = require('../controllers/user');
+const { isAuth } = require('../middlewares/auth');
+const {
+  validateUserSignUp,
+  userVlidation,
+  validateUserSignIn,
+} = require('../middlewares/validation/user');
+
+router.post('/create-user', validateUserSignUp, userVlidation, createUser);
+router.post('/sign-in', validateUserSignIn, userVlidation, userSignIn);
+router.post('/sign-out', isAuth, signOut);
+
+
+module.exports = router;
